68c69cc0bbc126560f9f1f2340fdb1d9d9dceeea
NicoDayo <106494279+NicoDayo@users.noreply.github.com>
2022-12-02 12:23:18 -0800

    Dark mode add to all except buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,32 @@
 
 const darkmodebutton = document.querySelector("#Darkmode")
-const DMbody = document.querySelector("body")
-const DMsidebar = document.querySelector('aside')
-const DMnoteheader = document.querySelector('h2')
-const DMtitleheader = document.querySelector('h1')
+const DM_body = document.querySelector('body')
 
+const DM_titleheader = document.querySelector('h1')
+const DM_headerlinks = document.querySelectorAll('#Ulist li a')
+
+const DM_sidebar = document.querySelector('.sidebar')
+const DM_noteheader = document.querySelector('h2')
+const DM_sidebullets = document.querySelector('ul')
+const DM_sidenotes = document.querySelectorAll('aside ul li a')
 
 
 
 function toggledark()
 {
-   DMbody.classList.toggle('dark')
-   DMtitleheader.classList.toggle('darkmodefont')
-   DMnoteheader.classList.toggle('darkmodefont')
-   DMsidebar.classList.toggle('dark3')
+   DM_body.classList.toggle('dark')
+   DM_titleheader.classList.toggle('darkmodefont')
+   DM_noteheader.classList.toggle('darkmodefont')
+   DM_sidebullets.classList.toggle('darkmodefont')
+   DM_sidebar.classList.toggle('dark3')
 
+      var headlink = DM_headerlinks
+      {for (var i=0; i < headlink.length; i++){
+      headlink[i].classList.toggle('darkmodefont')}} //goes through each a-tag in Ulist
+   
+      var sidelink = DM_sidenotes
+      {for (var i=0; i < sidelink.length; i++){
+      sidelink[i].classList.toggle('darkmodefont')}} //goes through each side note a-tag
+   
 }
 darkmodebutton.addEventListener('click', toggledark)
