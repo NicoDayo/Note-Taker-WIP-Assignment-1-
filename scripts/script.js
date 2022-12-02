41e628eda8cc403c699f6c45520f95cc636e2c17
NicoDayo <106494279+NicoDayo@users.noreply.github.com>
2022-12-02 14:32:22 -0800

    darkmode button colors modified

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,36 @@
-
-const darkmodebutton = document.querySelector("#Darkmode")
+// ====== DARK MODE CONSTANTS =============
+// main parts
+const darkmodebutton = document.querySelector(".Darkmode")
 const DM_body = document.querySelector('body')
 
+// Title header and links
 const DM_titleheader = document.querySelector('h1')
 const DM_headerlinks = document.querySelectorAll('#Ulist li a')
 
+// sidebar
 const DM_sidebar = document.querySelector('.sidebar')
 const DM_noteheader = document.querySelector('h2')
 const DM_sidebullets = document.querySelector('ul')
 const DM_sidenotes = document.querySelectorAll('aside ul li a')
 
-
+// buttons
+const DM_newnote = document.querySelector('.Newnote')
+const DM_darkmode = document.querySelector('.Darkmode')
+const DM_save = document.querySelector('.Save')
+const DM_cancel = document.querySelector('.Cancel')
 
 function toggledark()
 {
-   DM_body.classList.toggle('dark')
+   DM_body.classList.toggle('darkmain')
    DM_titleheader.classList.toggle('darkmodefont')
    DM_noteheader.classList.toggle('darkmodefont')
    DM_sidebullets.classList.toggle('darkmodefont')
-   DM_sidebar.classList.toggle('dark3')
+   DM_sidebar.classList.toggle('darksidebar')
+   DM_newnote.classList.toggle('dark_newnote')
+   DM_darkmode.classList.toggle('dark_darkmode')
+   DM_save.classList.toggle('dark_save')
+   DM_cancel.classList.toggle('dark_cancel')
+   
 
       var headlink = DM_headerlinks
       {for (var i=0; i < headlink.length; i++){
@@ -27,6 +39,7 @@ function toggledark()
       var sidelink = DM_sidenotes
       {for (var i=0; i < sidelink.length; i++){
       sidelink[i].classList.toggle('darkmodefont')}} //goes through each side note a-tag
+   
    
 }
 darkmodebutton.addEventListener('click', toggledark)
